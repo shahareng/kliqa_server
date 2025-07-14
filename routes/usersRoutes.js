244108a8b3c029b9308b73bfa815648d33a3aab5
meirGetz <meirgetz@gmail.com>
2025-07-14 19:36:48 +0300

    Add and update user entity (model, controller, service, routes)

---
+++ routes/usersRoutes.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const userService = require('../services/usersService');
+
+// Get all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await userService.getAllUsers();
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(error.code ?? 400).send(error.message);
+  }
+});
+
+// Get specific user by ID
+router.get('/:userId', async (req, res) => {
+  try {
+    const user = await userService.getUserById(req.params.userId);
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(error.code ?? 400).send(error.message);
+  }
+});
+
+// Create a new user
+router.post('/', async (req, res) => {
+  try {
+    const newUser = await userService.createUser(req.body);
+    res.status(201).send(newUser);
+  } catch (error) {
+    res.status(error.code ?? 400).send(error.message);
+  }
+});
+
+// Update user
+router.put('/:userId', async (req, res) => {
+  try {
+    const updatedUser = await userService.updateUser(req.params.userId, req.body);
+    res.status(200).send(updatedUser);
+  } catch (error) {
+    res.status(error.code ?? 400).send(error.message);
+  }
+});
+
+// Delete user
+router.delete('/:userId', async (req, res) => {
+  try {
+    await userService.deleteUser(req.params.userId);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(error.code ?? 400).send(error.message);
+  }
+});
+
+module.exports = router;
